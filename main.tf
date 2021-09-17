175c4f8944578055cad6cd2c72ed46f146d64f21
Carsten Duch <carsten@hashicorp.com>
2021-09-17 14:00:52 +0200

    added port

--- main.tf
+++ main.tf
@@ -92,6 +92,14 @@ resource "aws_security_group" "allow" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "For WebUI of Keycloak"
+    from_port   = 9990
+    to_port     = 9990
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
