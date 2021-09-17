64a054d6aa04ab1ab61397a34670957ed58d233a
Carsten Duch <carsten@hashicorp.com>
2021-09-17 13:57:37 +0200

    changed ports

--- main.tf
+++ main.tf
@@ -84,10 +84,11 @@ resource "aws_security_group" "allow" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
   ingress {
-    description = "For Webserver reachability"
-    from_port   = 80
-    to_port     = 80
+    description = "For WebUI of Keycloak"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
